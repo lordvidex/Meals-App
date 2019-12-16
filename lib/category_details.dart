e853ac9f5788b113d3f5ebb02a738c9c84feba64
Lordvidex <lordvidex.prime@gmail.com>
2019-12-16 12:44:45 +0300

    Dummy data added

--- lib/category_details.dart
+++ lib/category_details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy-data.dart';
+
 class CategoryDetails extends StatelessWidget {
   // final String categoryId;
   // final String categoryTitle;
@@ -7,17 +9,23 @@ class CategoryDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final properties = ModalRoute.of(context).settings.arguments as Map<String,String>;
+    final properties =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final String categoryId = properties['id'];
     final String categoryTitle = properties['title'];
+
+    final categoryItems = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
     return Scaffold(
-      appBar: AppBar(title: Text(categoryTitle),),
-      body: Center(
-        child: Text(
-          'These are the $categoryTitle recipes',
-          style: Theme.of(context).textTheme.body1,
-        ),
+      appBar: AppBar(
+        title: Text(categoryTitle),
       ),
+      body: ListView.builder(
+          itemCount: categoryItems.length,
+          itemBuilder: (ctx, index) {
+            return Text(categoryItems[index].title);
+          }),
     );
   }
 }
