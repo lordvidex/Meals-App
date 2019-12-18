f6ce2b6659aa783b8b29e04d593ee5a436f2cb96
lordvidex <lordvidex.prime@gmail.com>
2019-12-19 02:41:52 +0300

    Hitch with favorites screen fixed

--- lib/screens/category_meals_favorites_screen.dart
+++ lib/screens/category_meals_favorites_screen.dart
@@ -3,28 +3,43 @@ import 'package:flutter/material.dart';
 import '../models/meal.dart';
 import '../widgets/meal_item.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
   final List<Meal> favoriteMeals;
   const FavoritesScreen(this.favoriteMeals);
+
+  @override
+  _FavoritesScreenState createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  Widget listViewBuilder;
+  @override
+  void initState() {
+    setState(() => listViewBuilder = ListView.builder(
+        itemCount: widget.favoriteMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: widget.favoriteMeals[index].id,
+            title: widget.favoriteMeals[index].title,
+            affordability: widget.favoriteMeals[index].affordability,
+            complexity: widget.favoriteMeals[index].complexity,
+            duration: widget.favoriteMeals[index].duration,
+            imageUrl: widget.favoriteMeals[index].imageUrl,
+          );
+        }));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return favoriteMeals.isEmpty
+    return widget.favoriteMeals.isEmpty
         ? Container(
             child:
                 //TODO: complete with empty view screen
-                Center(child: Text('No Favorite meal selected..',style:Theme.of(context).textTheme.body1)),
+                Center(
+                    child: Text('No Favorite meal selected..',
+                        style: Theme.of(context).textTheme.body1)),
           )
-        : ListView.builder(
-            itemCount: favoriteMeals.length,
-            itemBuilder: (ctx, index) {
-              return MealItem(
-                id: favoriteMeals[index].id,
-                title: favoriteMeals[index].title,
-                affordability: favoriteMeals[index].affordability,
-                complexity: favoriteMeals[index].complexity,
-                duration: favoriteMeals[index].duration,
-                imageUrl: favoriteMeals[index].imageUrl,
-              );
-            });
+        : listViewBuilder;
   }
 }
